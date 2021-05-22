28abc65c9a696c596393fc688b3eb3255b946bd5
vinayak-keyvalue <63287676+vinayak-keyvalue@users.noreply.github.com>
2021-05-22 22:42:28 +0530

    Get port from env (#3)
    * Get port from env
    * Update env samples with most common values

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { CustomExceptionsFilter } from './exception/exception.filter';
+import { ConfigService } from '@nestjs/config';
 import { LoggerService } from './logger/logger.service';
 
 async function bootstrap() {
   const appOptions = { cors: true };
   const app = await NestFactory.create(AppModule, appOptions);
+
+  const configService = app.get(ConfigService);
   const logger = LoggerService.getInstance('bootstrap()');
   app.setGlobalPrefix('auth/api');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new CustomExceptionsFilter());
 
-  await app.listen(4000);
+  await app.listen(configService.get('PORT') || 4000);
 
   logger.info(`Application is running on: ${await app.getUrl()}`);
 }
