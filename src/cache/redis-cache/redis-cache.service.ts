660014151e555e1725056338873712da0de0f62d
Doyle Fermi <doyle@keyvalue.systems>
2021-06-04 12:30:06 +0530

    Initial bringup

---
+++ src/cache/redis-cache/redis-cache.service.ts
@@ -0,0 +1,23 @@
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+
+@Injectable()
+export class RedisCacheService {
+  constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
+
+  async get(key: any): Promise<any> {
+    return await this.cache.get(key);
+  }
+
+  async set(key: any, value: any) {
+    await this.cache.set(key, value);
+  }
+
+  async reset() {
+    await this.cache.reset();
+  }
+
+  async del(key: any) {
+    await this.cache.del(key);
+  }
+}
