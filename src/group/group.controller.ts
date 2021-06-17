353f27cf7b168c4d5b546cade8038efc030c9280
johns_keyvalue <johns@keyvalue.systems>
2021-06-17 08:43:28 +0530

    Corrections

--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -1,29 +1,29 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { Args } from '@nestjs/graphql';
 import Group from 'src/authorization/entity/group.entity';
 import { GroupService } from './group.service';
 
-@Controller('group')
+@Controller('groups')
 export class GroupController {
 
     constructor(private groupService: GroupService){}
 
-
-    @Get()
+// GET /groups/profile
+    @Get('profile')
     async getGroups(){
-            this.groupService.getGroups
+            return this.groupService.getGroups
     }
 
 
 
     @Post()
-    async createGroup(@Body() body: Group){
-            this.groupService.createGroup(body)
+    async createGroup(@Body() body: Group, @Req() req: Request){
+        return this.groupService.createGroup(body)
     }
 
 
     @Get(':id')
-    async getGroup(@Args('id') id: string){
-        this.groupService.getGroup(id)
+    async getGroup(@Param('id') id: string){
+        return  this.groupService.getGroup(id)
     }
 }
--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class GroupService {
 
+    data = [{ id: '1', name: 'ceo' }];
 
     async getGroups(){
         return [];
