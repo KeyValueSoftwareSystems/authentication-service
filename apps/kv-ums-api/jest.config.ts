cdbd704870cbc3283b6bcb227ede6d897c908794
Doyle Fermi <doyle@keyvalue.systems>
2022-11-14 22:57:07 +0530

    add: init nest workspace

---
+++ apps/kv-ums-api/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'kv-ums-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/kv-ums-api',
+};
